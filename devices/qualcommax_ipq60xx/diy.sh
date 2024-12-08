910dd53dcf6650e76297e1a9a37038f1c49fa44d
FreeJ233 <50964071+FreeJ233@users.noreply.github.com>
2024-12-08 19:09:56 +0800

    Update diy.sh

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -8,6 +8,12 @@ rm -rf package/firmware package/boot/uboot-envtools target/linux/qualcommax
 
 git_clone_path openwrt-24.10 https://github.com/LiBwrt-op/openwrt-6.x package/firmware package/boot/uboot-envtools target/linux/qualcommax
 
-wget -N https://github.com/openwrt/openwrt/raw/refs/heads/openwrt-24.10/target/linux/qualcommax/ipq60xx/target.mk -P target/linux/qualcommax/ipq60xx/
+wget -N https://github.com/openwrt/openwrt/raw/refs/heads/openwrt-24.-/target/linux/qualcommax/ipq60xx/target.mk -P target/linux/qualcommax/ipq60xx/
 
 rm -rf target/linux/qualcommax/patches-6.6/06*-qca-*.patch
+
+#安装luci-app-daed
+git clone https://github.com/QiuSimons/luci-app-daed package/dae
+
+#get the libcron （daed依赖）
+mkdir -p Package/libcron && wget -O Package/libcron/Makefile https://raw.githubusercontent.com/immortalwrt/packages/refs/heads/master/libs/libcron/Makefile
